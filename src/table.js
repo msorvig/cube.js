da564515620b923d6381a16c5451ee7306a63922
Morten Sørvig <msorvig@Solgunns-Mac.local>
2013-05-24 20:40:31 +0200

    Objectify table

--- src/table.js
+++ src/table.js
@@ -13,60 +13,74 @@
     }
 */
 
-function makeFields(ids, labels, kinds) {
-    var definedIds = ids // id is mandatory
-    var definedLabels = labels || [] // optional
-    var definedkinds = kinds || [] // optional
-    var fields = []
-
-    for (var i = 0; i < definedIds.length; ++i) {
-        fields.push({ "id" : definedIds[i],
-                      "label" : definedLabels[i],
-                      "kind" : definedkinds[i] })
-    }
-    return fields
-}
 
 function makeTable(ids, labels, kinds) {
-    return {
-        "schema" : {
-            "fields" : makeFields(ids, labels, kinds)
-        },
+    var table = {
+        "schema" : { "fields" : makeFields(ids, labels, kinds) },
         "rows" : []
+    }
+
+    function makeFields(ids, labels, kinds) {
+        var definedIds = ids // id is mandatory
+        var definedLabels = labels || [] // optional
+        var definedkinds = kinds || [] // optional
+        var fields = []
+
+        for (var i = 0; i < definedIds.length; ++i) {
+            fields.push({ "id" : definedIds[i],
+                          "label" : definedLabels[i],
+                          "kind" : definedkinds[i] })
+        }
+        return fields
+    }
+
+    function addRow(row) {
+        table.rows.push(row)
+    }
+
+    function rowCount() {
+        return table.rows.length
+    }
+
+    function row(index) {
+        return table.rows[index]
+    }
+
+    function fieldAttributes(attribute) {
+        return table.schema.fields.map(
+            function(field){ return field[attribute] }
+        )
+    }
+
+    function fieldIds() {
+        return fieldAttributes("id")
+    }
+
+    function fieldLabels() {
+        return fieldAttributes("label")
+    }
+
+    function fieldKinds() {
+        return fieldAttributes("kind")
+    }
+
+    function filterFields(fieldIds) {
+        return table.schema.fields.filter(
+            function(field) { return (fieldIds.indexOf(field) != -1) }
+        )
+    }
+
+    return {
+        "addRow" : addRow,
+        "rowCount" : rowCount,
+        "row" : row,
+        "fieldIds" : fieldIds,
+        "fieldLabels" : fieldLabels,
+        "fieldKinds" : fieldKinds,
+        "filterFields" : filterFields,
     }
 }
 
-function addRow(table, row) {
-    table.rows.push(row)
-}
 
-function rowCount(table) {
-    return table.rows.length
-}
 
-function row(table, index) {
-    return table.rows[index]
-}
 
-function fieldAttributes(table, attribute) {
-    return table.schema.fields.map(
-        function(field){ return field[attribute] }
-    )
-}
-
-function fieldIds(table) {
-    return fieldAttributes(table, "id")
-}
-
-function fieldLabels(table) {
-    return fieldAttributes(table, "label")
-}
-
-function fieldKinds(table) {
-    return fieldAttributes(table, "kind")
-}
-function filterFields(table, fieldIds) {
-    return table.schema.fields.filter(
-        function(field) { return (fieldIds.indexOf(field) != -1) }
-    )
-}
--- tests/table2/tst_table.js
+++ tests/table2/tst_table.js
@@ -2,9 +2,8 @@ test("columns", function() {
     var ids = ["Foo", "Bar", "Baz"]
     var table = makeTable(ids)
     ok(table)
-    deepEqual(fieldIds(table) , ids)
-});
-
+    deepEqual(table.fieldIds(), ids)
+})
 
 test("rows", function() {
     var ids = ["Foo", "Bar", "Baz"]
@@ -14,15 +13,14 @@ test("rows", function() {
     var row1 = ["2", "B", "2.71"]
     var row2 = ["2", "B", "6.62"]
 
-    equal(rowCount(table), 0)
+    equal(table.rowCount(), 0)
 
-    addRow(table, row0)
-    addRow(table, row1)
-    addRow(table, row2)
+    table.addRow(row0)
+    table.addRow(row1)
+    table.addRow(row2)
 
-    equal(rowCount(table), 3)
-    equal(row(table, 0), row0)
-    equal(row(table, 1), row1)
-    equal(row(table, 2), row2)
-
-});
+    equal(table.rowCount(), 3)
+    equal(table.row(0), row0)
+    equal(table.row(1), row1)
+    equal(table.row(2), row2)
+})
